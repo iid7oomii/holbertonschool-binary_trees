4a061b770ca8727f451048dbabdd1626257b3983
Abdulrahman Alghamdi <A.D.Alghamdi@outlook.com>
2025-10-27 15:11:44 +0300

    add 9 fuctions to binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -23,4 +23,22 @@ void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+void binary_tree_delete(binary_tree_t *tree);
+
+int binary_tree_is_root(const binary_tree_t *node);
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+
+size_t binary_tree_height(const binary_tree_t *tree);
+
+size_t binary_tree_size(const binary_tree_t *tree);
+
+size_t binary_tree_nodes(const binary_tree_t *tree);
+
+int binary_tree_is_full(const binary_tree_t *tree);
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
 #endif
