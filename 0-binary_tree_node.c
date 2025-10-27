72d46ec04242ad4ddb000e1e36666ff186dea354
Your Name <you@example.com>
2025-10-27 14:52:53 +0300

    fix bettey style

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -17,7 +17,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	node->parent = parent;
 	node->left = NULL;
-	node->right = NULL;   
-	node->n = value; 
+	node->right = NULL;
+	node->n = value;
 	return (node);
 }
