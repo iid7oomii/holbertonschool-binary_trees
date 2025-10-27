0c4e2e0c411fb3a706c0ec88804227c983b707aa
Your Name <you@example.com>
2025-10-27 16:51:18 +0300

    add binary_tree_is_leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,6 +10,12 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    int number = 0;
-    binary_tree_t 
+	if (node == NULL)
+		return (0);
+	else if (node->right != NULL || node->left != NULL)
+		return (0);
+	else if (node->right == NULL && node->left == NULL)
+		return (1);
+
+	return (0);
 }
